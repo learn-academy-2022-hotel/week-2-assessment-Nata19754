c83d46b28f99d0cf33b1f11455e4869d99019581
Nata19754 <chavesnatally0419@gmail.com>
2022-12-10 19:55:59 -0800

    completed assessment

--- code-challenges.test.js
+++ code-challenges.test.js
@@ -1,5 +1,7 @@
 // ASSESSMENT 2: Coding Practical Questions with Jest
 
+// const { exportAllDeclaration } = require("@babel/types")
+
 // Please read all questions thoroughly
 // Pseudo coding is REQUIRED
 // If you get stuck, please leave comments to help us understand your thought process
@@ -14,10 +16,10 @@
 
 // a) Create a test with expect statements for each of the variables provided.
 
-const numbersArray1 = [6, 7, 8, 9, 10]
-// Expected output: [18, 21, 24, 27, 30]
-const numbersArray2 = [24, 27, 30, 33, 36]
-// Expected output: [72, 81, 90, 99, 108]
+// const numbersArray1 = [6, 7, 8, 9, 10]
+// // Expected output: [18, 21, 24, 27, 30]
+// const numbersArray2 = [24, 27, 30, 33, 36]
+// // Expected output: [72, 81, 90, 99, 108]
 
 // b) Create the function that makes the test pass.
 
@@ -25,35 +27,229 @@ const numbersArray2 = [24, 27, 30, 33, 36]
 
 // a) Create a test with expect statements for each of the variables provided.
 
+// const object1 = { number: 15 }
+// // Expected output: "15 is divisible by three"
+// const object2 = { number: 0 }
+// // Expected output: "0 is divisible by three"
+// const object3 = { number: -7 }
+// // Expected output: "-7 is not divisible by three"
+
 const object1 = { number: 15 }
-// Expected output: "15 is divisible by three"
 const object2 = { number: 0 }
-// Expected output: "0 is divisible by three"
 const object3 = { number: -7 }
-// Expected output: "-7 is not divisible by three"
+
+describe ("divisible", () => {
+    it("takes a object as an argument and decides if the number inside it is evenly divisible by three or not",() => {
+    expect (divisible(object1)).toEqual(("15 is divisible by three"))
+    expect (divisible(object2)).toEqual(("0 is divisible by three"))
+    expect (divisible(object3)).toEqual(("-7 is not divisible by three"))
+    })
+})
+
+//GOOD FAIL:
+
+// FAIL  ./code-challenges.test.js
+// divisible
+//   ✕ takes a object as an argument and decides if the number inside it is evenly divisible by three or not (1 ms)
+
+// ● divisible › takes a object as an argument and decides if the number inside it is evenly divisible by three or not
+
+//   ReferenceError: divisible is not defined
+
+
 
 // b) Create the function that makes the test pass.
+
+//PSEUDOCODE:
+//Create function called divisible
+//input: objest as an argument
+//output: string stating if the number in the object is divisible by 3 or not
+//use conditionals in function
+//if % 3 of the value is strickly equal to zero, then the number is divisible by 3
+//else, the value is not divisible by 3
+//callback function on the 3 variable and run $yarn jest for test
+
+
+const divisible = (value) => {
+    if(value.number % 3 === 0) {
+        return `${value.number} is divisible by three`
+    }
+    else {
+        return `${value.number} is not divisible by three`
+    }
+   
+}
+console.log(divisible(object1))
+//OUTPUT: 15 is divisible by three
+console.log(divisible(object2))
+//OUTPUT: 0 is divisible by three
+console.log(divisible(object3))
+//OUTPUT: -7 is not divisible by three
+
+// 15 is divisible by three
+
+// at Object.log (code-challenges.test.js:80:9)
+
+// console.log
+// 0 is divisible by three
+
+// at Object.log (code-challenges.test.js:82:9)
+
+// console.log
+// -7 is not divisible by three
+
+// at Object.log (code-challenges.test.js:84:9)
+
+// PASS  ./code-challenges.test.js
+// divisible
+// ✓ takes a object as an argument and decides if the number inside it is evenly divisible by three or not (1 ms)
+
+
 
 // --------------------2) Create a function that takes in an array of words and returns an array with all the words capitalized.
 
 // a) Create a test with expect statements for each of the variables provided.
 
 const randomNouns1 = ["streetlamp", "potato", "teeth", "conclusion", "nephew"]
-// Expected output: ["Streetlamp", "Potato", "Teeth", "Conclusion", "Nephew"]
+// // // Expected output: ["Streetlamp", "Potato", "Teeth", "Conclusion", "Nephew"]
 const randomNouns2 = ["temperature", "database", "chopsticks", "mango"]
-// Expected output: ["Temperature", "Database", "Chopsticks", "Mango"]
+// // // Expected output: ["Temperature", "Database", "Chopsticks", "Mango"]
+
+describe ("cap", () => {
+        it("takes in an array of words and returns an array with all the words capitalized",() => {
+        expect (cap(randomNouns1)).toEqual(["Streetlamp", "Potato", "Teeth", "Conclusion", "Nephew"])
+        expect (cap(randomNouns2)).toEqual(["Temperature", "Database", "Chopsticks", "Mango"])
+        })
+    })
+
+//GOOD FAIL:
+
+// FAIL  ./code-challenges.test.js
+// capitalized
+//   ✕ takes in an array of words and returns an array with all the words capitalized
+
+// ● capitalized › takes in an array of words and returns an array with all the words capitalized
+
+//   ReferenceError: capitalized is not defined
+
+
 
 // b) Create the function that makes the test pass.
+
+//PSEUSO CODE
+//Create a function called capitalized
+//input: array of strings
+//output: array with all the words capitalized
+//use map to iterate
+//after map method has been implemented, use charAt to find the character at index 0 of each element and use toUppercase on the character at index 0
+//slice the rest of the remaining characters in the elements and apply the tolowercase
+
+function cap(array) {
+    return array.map(placement => {
+        return placement.charAt(0).toUpperCase() + placement.slice(1).toLowerCase()
+    });
+    };
+console.log(cap(randomNouns1))
+//OUTPUT: [ 'Streetlamp', 'Potato', 'Teeth', 'Conclusion', 'Nephew' ]
+console.log(cap(randomNouns2))
+//OUTPUT: [ 'Temperature', 'Database', 'Chopsticks', 'Mango' ]
+
+
+//GOOD TEST
+
+// [ 'Streetlamp', 'Potato', 'Teeth', 'Conclusion', 'Nephew' ]
+
+// at Object.log (code-challenges.test.js:149:9)
+
+// console.log
+// [ 'Temperature', 'Database', 'Chopsticks', 'Mango' ]
+
+// at Object.log (code-challenges.test.js:150:9)
+
+// PASS  ./code-challenges.test.js
+// cap
+// ✓ takes in an array of words and returns an array with all the words capitalized (1 ms)
+
+
 
 // --------------------3) Create a function that takes in a string and logs the index of the first vowel.
 
 // a) Create a test with expect statements for each of the variables provided.
 
+// const vowelTester1 = "learn"
+// // Expected output: 1
+// const vowelTester2 = "academy"
+// // Expected output: 0
+// const vowelTester3 = "challenges"
+// // Expected output: 2
+
 const vowelTester1 = "learn"
-// Expected output: 1
 const vowelTester2 = "academy"
-// Expected output: 0
 const vowelTester3 = "challenges"
-// Expected output: 2
+
+describe ("fVowel",() => {
+    it ("takes in a string and logs the index of the first vowel",() => {
+    expect(fVowel(vowelTester1)).toEqual(1)
+    expect(fVowel(vowelTester2)).toEqual(0)
+    expect(fVowel(vowelTester3)).toEqual(2)
+
+    })
+})
+
+
+//GOOD FAIL
+
+// FAIL  ./code-challenges.test.js
+// fVowel
+//   ✕ takes in a string and logs the index of the first vowel
+
+// ● fVowel › takes in a string and logs the index of the first vowel
+
+//   ReferenceError: fVowel is not defined
+
 
 // b) Create the function that makes the test pass.
+
+//PSEUDOCODE
+//Create a function CALLED FVowel
+//input: "string"
+//output: index of first vowel
+//use for loop
+//use charAt and match to find if character at index matches the vowels
+//retuen the index if character matches
+
+const fVowel = (string) => {
+    for(let i=0; i<string.length; i++){
+        if (string.charAt(i).match(/[a,e,i,o,u]/)) {
+            return i
+        }
+    }
+}
+console.log(fVowel(vowelTester1))
+// //output:1
+console.log(fVowel(vowelTester2))
+// //output:0
+console.log(fVowel(vowelTester3))
+// //output:2
+
+//PASSING TEST
+
+// 1
+
+// at Object.log (code-challenges.test.js:203:9)
+
+// console.log
+// 0
+
+// at Object.log (code-challenges.test.js:204:9)
+
+// console.log
+// 2
+
+// at Object.log (code-challenges.test.js:205:9)
+
+// PASS  ./code-challenges.test.js
+// fVowel
+// ✓ takes in a string and logs the index of the first vowel (1 ms)
+
+
--- interview-questions.md
+++ interview-questions.md
@@ -8,50 +8,86 @@ Then, research the question to expand on your answer. Even if you feel you have 
 
 1. What is the difference between a parameter and an argument?
 
-Your answer:
+
+Your answer: 
+
+A parameter is a place holder for an input function aregument, while an argument is the actual value.
 
 Researched answer:
+
+Parameter is a place holder passed into a function that are then assigned a value called an argument. You can remember what a parameter is by thinking that they are the aliases for the values that will eventually be passed.On the other hand, arguments are the actual value or content passed to the function when its invoked.You can think of them as the real deal (the actual information that will be inputed)
+
+For example in the function below, the parameter is (a,b), while the argument is ( 3 , 4)
+
+function addNumbers (a , b) {
+    const adding = a + b ;// 3  + 4
+    return adding;
+}
+console.log(adding(3 , 4));
+
 
 2. The JavaScript built in method .map() takes predefined parameters. What are they? Which are required and which are optional?
 
-Your answer:
+Your answer: 
 
-Researched answer:
+The predefined parameters that the built in method .map takes are value,array,function,index, and this value. The required parameter is value, while array and index are optional depending on the developers preference.
+
+Researched answer: 
+
+.map takes in two arguments.Fuction(currentvalue,Index, arr) is a required parameter, and it runs on each individual element of an array. The function parameter contains 3 parameters. The first parameter (currents value) is required and contains the value of the current elements. The next 3 are optional, they are the index parameter , the array (arr) parameter, and the thisValue parameter.The index holds the index of the currents element,the arr holds the array, and the thisValue holds the value passed to the function that will be used as the this value. 
+
+
 
 3. What is the difference between map and filter?
 
-Your answer:
+Your answer: 
+
+The map method goes through an array, and returns a new updated array with the condition that you have requested applied. Keep in mind, that the map method keeps the same length as the original array and does not change the original array. The filter method goes through the array and filters out specific elements from the original array, and creates a new array with the elements that met the condition that you applied.
 
 Researched answer:
+
+The difference between map and filter would be the outputs that are returned after each one is performed. The map method returns the result of each element of an array having passed through a function, the filter method, requires the function that is applied to the array, to return with a boolean value, either true or false. It is then able to pass each element through the function, creating a new array with the filtered elements.
+
+
 
 4. What is iteration?
 
-Your answer:
+Your answer: 
+
+Iteration is the ability of a process to be able to go over and over again until a specific condition that you have assigned has been met.
 
 Researched answer:
+
+ Iteration is a means for a programmers to be able to iterate or go through over a collection or data structure repeatedlyuntil a specific condition has been met. Iteration helps to pull information from  specific source in a one in a time fashion. For example, if you have a bowl of m&ms that include all the colors, but you only want the blue ones, then you will start the process of taking out the m&ms that are not blue. With repeating the process of taking the non blue m&ms out, you are experiencing iteration, because you are going through the process until you have a bowl full of only blue m&ms. This example applies to coding. If you have an array of numbers but you only want the even numbers, then you can use iteration to have that set condition that you want(only even numbers)met.In todays world, iteration is used to perform specific task,solve problem, and present solutions.
 
 5. What is the difference between a class and an object?
 
-Your answer:
+Your answer: 
+
+A class is the format or blueprint that describes the way an object is created. On the other hand, an object is more like a collection of data made up of keys and values. 
 
 Researched answer:
+
+The difference between class and and an object, is that a class defines the object properties and describes the object behavior. A class can be called the "blueprint" or "template" from which object will be created. An object is a stand alone entity that has properties and types.  An example of classes and objects can be a dog being a class and color, breed, barking, and whinnig being objects. A class is a collection of object and it is defined only once. An object can also be created as many as there is a requirement for.
 
 6. STRETCH: What is hoisting in JavaScript?
 
-Your answer:
+Your answer: A form of simplifying a function by moving a piece of code to the top of a file.
 
-Researched answer:
+Researched answer: 
+
+Hoisting in JavaScript  can be defined as the action or behavior of moving all the declarations at the top of files. Hoisting allows programmers to use functions and variables prior to them being declared. To sum this up in simpler words, this means that no matter where we declare the variables,functions, or even classes it can get moved to the top of their scope.
 
 ## Looking Ahead: Terms for Next Week
 
 Research and define the following terms to the best of your ability.
 
-1. User stories:
+1. User stories: Tool asset that allows us to obtain a simplified description of what an individual wants
 
-2. Spread operator:
+2. Spread operator: Is used to take an array and expanded into individual elements. Makes it easier to convert an array into a string and apply certain methods like the toUppercase. ( Was used in previous assignment)
 
-3. React state:
+3. React state: used to store data or informtion about components that allows us to be able to manage data in applications
 
-4. React props:
+4. React props: The React version of function arguments. They are the area where the properties of an object are stored.
 
-5. DOM events:
+5. DOM events: Events that are triggered  due to user action or by browser that signal that something has happened or is actively happening.
